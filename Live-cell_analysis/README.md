6252496af95da83ae2ffb8489b1fa88ad3ebbe58
tsabate <56267464+tsabate@users.noreply.github.com>
2024-07-26 16:26:14 +0200

    Added 3D polymer simulation files

--- Live-cell_analysis/README.md
+++ Live-cell_analysis/README.md
@@ -3,4 +3,4 @@
 This section contains two repositories for:
 
 1. Localization and tracking of 2 fluorescent spots in different channels from live-cell microscopy images (Image processing)
-2. Data analysis of distance time series (Data analysis)
+2. Quantification of distance time series (Quantification)
---
+++ Polymer_simulations/LAMMPS_3D_polymer_simulations/run_Multiple.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+for i in {1..50}
+do
+   mkdir set$i
+done
+
+for i in {1..50}
+do
+   cp simIndex=$((i-1)).in set$i
+   cp interactions set$i
+   cp initial.py set$i
+   cp input_data_generate_f1.py set$i
+   cd set$i/
+   python3 initial.py > initial_conformation.txt
+   python3 input_data_generate_f1.py > param
+   sbatch -c 1 --qos=specific --job-name=L2P1100C8$i  -t 5-00:00:00 --mem 2G --wrap='lmp_serial -in simIndex='$((i-1))'.in &> chain.txt'
+   cd ../
+done
